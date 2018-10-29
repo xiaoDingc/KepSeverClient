c24cfbe83609f5cac787e04d982d7c09beda6ad0
DHX\dc <771801383@qq.com>
2018-10-29 15:13:27 +0800

    Init

---
+++ KepCom/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace KepCom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tsslblServerState = new System.Windows.Forms.Label();
+            this.tsslblItemCount = new System.Windows.Forms.Label();
+            this.tsslblGroupCount = new System.Windows.Forms.Label();
+            this.tvwGroupList = new System.Windows.Forms.TreeView();
+            this.opcServercontextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.opcGroupcontextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstviewItems = new System.Windows.Forms.ListView();
+            this.lbShow = new System.Windows.Forms.Label();
+            this.dgvShowValue = new System.Windows.Forms.DataGridView();
+            this.Tag = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TimeStap = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShowValue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tsslblServerState
+            // 
+            this.tsslblServerState.AutoSize = true;
+            this.tsslblServerState.Location = new System.Drawing.Point(61, 28);
+            this.tsslblServerState.Name = "tsslblServerState";
+            this.tsslblServerState.Size = new System.Drawing.Size(41, 12);
+            this.tsslblServerState.TabIndex = 0;
+            this.tsslblServerState.Text = "label1";
+            // 
+            // tsslblItemCount
+            // 
+            this.tsslblItemCount.AutoSize = true;
+            this.tsslblItemCount.Location = new System.Drawing.Point(344, 28);
+            this.tsslblItemCount.Name = "tsslblItemCount";
+            this.tsslblItemCount.Size = new System.Drawing.Size(41, 12);
+            this.tsslblItemCount.TabIndex = 1;
+            this.tsslblItemCount.Text = "label2";
+            // 
+            // tsslblGroupCount
+            // 
+            this.tsslblGroupCount.AutoSize = true;
+            this.tsslblGroupCount.Location = new System.Drawing.Point(576, 28);
+            this.tsslblGroupCount.Name = "tsslblGroupCount";
+            this.tsslblGroupCount.Size = new System.Drawing.Size(41, 12);
+            this.tsslblGroupCount.TabIndex = 2;
+            this.tsslblGroupCount.Text = "label3";
+            // 
+            // tvwGroupList
+            // 
+            this.tvwGroupList.Location = new System.Drawing.Point(54, 101);
+            this.tvwGroupList.Name = "tvwGroupList";
+            this.tvwGroupList.Size = new System.Drawing.Size(252, 175);
+            this.tvwGroupList.TabIndex = 3;
+            this.tvwGroupList.MouseDown += new System.Windows.Forms.MouseEventHandler(this.tvwGroupList_MouseDown);
+            // 
+            // opcServercontextMenuStrip
+            // 
+            this.opcServercontextMenuStrip.Name = "opcServercontextMenuStrip";
+            this.opcServercontextMenuStrip.Size = new System.Drawing.Size(61, 4);
+            // 
+            // opcGroupcontextMenuStrip
+            // 
+            this.opcGroupcontextMenuStrip.Name = "opcGroupcontextMenuStrip";
+            this.opcGroupcontextMenuStrip.Size = new System.Drawing.Size(61, 4);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(549, 64);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "开始加载";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstviewItems
+            // 
+            this.lstviewItems.Location = new System.Drawing.Point(312, 101);
+            this.lstviewItems.Name = "lstviewItems";
+            this.lstviewItems.Size = new System.Drawing.Size(281, 175);
+            this.lstviewItems.TabIndex = 7;
+            this.lstviewItems.UseCompatibleStateImageBehavior = false;
+            this.lstviewItems.View = System.Windows.Forms.View.List;
+            this.lstviewItems.SelectedIndexChanged += new System.EventHandler(this.lstviewItems_SelectedIndexChanged);
+            // 
+            // lbShow
+            // 
+            this.lbShow.AutoSize = true;
+            this.lbShow.Location = new System.Drawing.Point(54, 322);
+            this.lbShow.Name = "lbShow";
+            this.lbShow.Size = new System.Drawing.Size(41, 12);
+            this.lbShow.TabIndex = 9;
+            this.lbShow.Text = "label1";
+            // 
+            // dgvShowValue
+            // 
+            this.dgvShowValue.AllowUserToOrderColumns = true;
+            this.dgvShowValue.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvShowValue.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Tag,
+            this.Value,
+            this.TimeStap});
+            this.dgvShowValue.Location = new System.Drawing.Point(599, 101);
+            this.dgvShowValue.Name = "dgvShowValue";
+            this.dgvShowValue.RowTemplate.Height = 23;
+            this.dgvShowValue.Size = new System.Drawing.Size(347, 175);
+            this.dgvShowValue.TabIndex = 10;
+            // 
+            // Tag
+            // 
+            this.Tag.HeaderText = "Tag";
+            this.Tag.Name = "Tag";
+            // 
+            // Value
+            // 
+            this.Value.HeaderText = "Value";
+            this.Value.Name = "Value";
+            // 
+            // TimeStap
+            // 
+            this.TimeStap.HeaderText = "TimeStap";
+            this.TimeStap.Name = "TimeStap";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 511);
+            this.Controls.Add(this.dgvShowValue);
+            this.Controls.Add(this.lbShow);
+            this.Controls.Add(this.lstviewItems);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.tvwGroupList);
+            this.Controls.Add(this.tsslblGroupCount);
+            this.Controls.Add(this.tsslblItemCount);
+            this.Controls.Add(this.tsslblServerState);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShowValue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label tsslblServerState;
+        private System.Windows.Forms.Label tsslblItemCount;
+        private System.Windows.Forms.Label tsslblGroupCount;
+        private System.Windows.Forms.TreeView tvwGroupList;
+        private System.Windows.Forms.ContextMenuStrip opcServercontextMenuStrip;
+        private System.Windows.Forms.ContextMenuStrip opcGroupcontextMenuStrip;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListView lstviewItems;
+        private System.Windows.Forms.Label lbShow;
+        private System.Windows.Forms.DataGridView dgvShowValue;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tag;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TimeStap;
+    }
+}
+
---
+++ KepCom/Form1.cs
@@ -0,0 +1,276 @@
+﻿using OPCAutomation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KepCom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region 私有变量
+
+        private OPCServer kepServer = new OPCServer();
+        private OPCBrowser opcBrowser;
+        private Dictionary<string, OPCGroup> groupMap = new Dictionary<string, OPCGroup>();
+        private Dictionary<int, OPCItem> itemMap = new Dictionary<int, OPCItem>();
+        #endregion
+        //此方法连接OPCServer上的一个Program.
+        public bool Connect(string host, string prog)
+        {
+            try
+            {
+                if (kepServer.ServerState == (int)OPCServerState.OPCRunning)
+                {
+                    if (this.kepServer != null)
+                    {
+                        this.kepServer.Disconnect();
+                        //this.connected = false;
+                    }
+                }
+                this.tvwGroupList.Nodes.Clear();
+                this.groupMap.Clear();
+                this.itemMap.Clear();
+                this.kepServer.Connect(prog, host);
+                this.tsslblServerState.Text = "OPC服务[" + prog + "]已连接";
+                TreeNode node = this.tvwGroupList.Nodes.Add(prog);
+                node.Tag = this.kepServer;
+                this.opcBrowser = this.kepServer.CreateBrowser();
+                LoadDataToTree(node, "");
+                this.SetItemsClientHandle();
+                //this.LoadConfig(prog);
+                this.tsslblItemCount.Text = "Item Count " + this.itemMap.Count.ToString();
+                this.tsslblGroupCount.Text = "Group Count " + this.groupMap.Count.ToString();
+                //this.connected = true;
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Error(ex);
+            }
+            return true;
+        }
+
+        //用来标识每一个OPCItem数据的唯一标识
+        private void SetItemsClientHandle()
+        {
+            foreach (OPCItem item in this.itemMap.Values)
+            {
+                item.ClientHandle = item.GetHashCode();
+            }
+        }
+
+        //此方法通过OPCBrowser将数据结节加载到TreeView中.
+        private bool LoadDataToTree(TreeNode node, string nodeName)
+        {
+            TreeNode childNode = null;
+            OPCGroup group = null;
+            try
+            {
+                this.opcBrowser.ShowBranches();
+                int count = this.opcBrowser.Count;
+                if (count == 0)
+                {
+                    this.opcBrowser.ShowLeafs(true);
+                    group = this.kepServer.OPCGroups.Add(nodeName);
+                    group.UpdateRate = 1000;
+                    group.IsSubscribed = true;
+                    group.IsActive = true;
+                    node.Tag = group;
+                    this.groupMap.Add(nodeName, group);
+                }
+                HashSet<string> itemList = new HashSet<string>();
+                foreach (object turn in this.opcBrowser)
+                {
+                    string path = nodeName;
+                    string name = turn.ToString();
+
+                    if (count != 0)
+                    {
+                        childNode = node.Nodes.Add(name);
+                        if (path != "")
+                        {
+                            path += ".";
+                        }
+                        path += name;
+                        this.opcBrowser.MoveDown(name);
+                        LoadDataToTree(childNode, path);
+                        this.opcBrowser.MoveUp();
+                    }
+                    else
+                    {
+                        OPCItem opcItem = group.OPCItems.AddItem(name, 0);
+                        itemList.Add(name);
+                        if (!this.itemMap.ContainsKey(opcItem.GetHashCode()))
+                        {
+                            this.itemMap.Add(opcItem.GetHashCode(), opcItem);
+                        }
+                    }
+                }
+
+                this.opcBrowser.ShowLeafs(false);
+                List<string> leafItems = new List<string>();
+                foreach (object t in this.opcBrowser)
+                {
+                    string itemId = this.opcBrowser.GetItemID(t.ToString());
+                    if (itemList.Contains(itemId))
+                    {
+                        continue;
+                    }
+                    leafItems.Add(itemId);
+                }
+
+                if (leafItems.Count != 0)
+                {
+                    group = this.kepServer.OPCGroups.Add(nodeName);
+                    group.UpdateRate = 1000;
+                    group.IsSubscribed = true;
+                    group.IsActive = true;
+                    this.groupMap.Add(nodeName, group);
+                    node.Tag = group;
+                    foreach (string t in leafItems)
+                    {
+                        OPCItem opcItem = group.OPCItems.AddItem(t, 0);
+                        //childNode = node.Nodes.Add(t.ToString());
+                        if (!this.itemMap.ContainsKey(opcItem.GetHashCode()))
+                        {
+                            this.itemMap.Add(opcItem.GetHashCode(), opcItem);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Error(ex);
+            }
+            return true;
+        }
+
+        private ListViewItem listitem;
+        //此方法将选中的叶子节点(OPCGroup)下的所有OPCItem显示在ListView中.
+        private void tvwGroupList_MouseDown(object sender, MouseEventArgs e)
+        {
+            try
+            {
+
+                TreeNode crtNode = this.tvwGroupList.GetNodeAt(e.X, e.Y);
+                TreeNode oldNode = this.tvwGroupList.SelectedNode;
+                if (oldNode != null)
+                {
+                    this.lstviewItems.Items.Clear();
+                    if (oldNode.Tag is OPCGroup)
+                    {
+                        OPCGroup old_group = oldNode.Tag as OPCGroup;
+                    }
+                }
+                this.tvwGroupList.SelectedNode = crtNode;
+
+                if (e.Button == MouseButtons.Right)
+                {
+                    if (crtNode.Tag is OPCServer)
+                    {
+                        this.opcServercontextMenuStrip.Show(this.tvwGroupList, new Point(e.X, e.Y));
+                    }
+                    else if (crtNode.Tag is OPCGroup)
+                    {
+                        this.opcGroupcontextMenuStrip.Show(this.tvwGroupList, new Point(e.X, e.Y));
+                    }
+                }
+                if (crtNode.Tag is OPCGroup)
+                {
+                    OPCGroup crt_group = crtNode.Tag as OPCGroup;
+                    OPCItems items = crt_group.OPCItems;
+                    if (items.Count != 0)
+                    {
+                        foreach (OPCItem item in items)
+                        {
+                            string itemId = item.ItemID;
+                            string itemValue = Convert.ToString(item.Value);
+                            
+                            if (item.CanonicalDataType == 11)
+                            {
+                                itemValue = Convert.ToInt32(Convert.ToBoolean(itemValue)).ToString();
+                            }
+                            listitem = new ListViewItem(new string[] {
+                    itemId,
+                    //item.CanonicalDataType.ToString(),
+                    itemValue,
+                    item.TimeStamp.ToLocalTime().ToString(),
+                    //OPCEnumHelper.GetQuality((OPCQuality)item.Quality),
+                    //"0"
+                            });
+                            
+                            listitem.Tag = item.ClientHandle;
+                            this.lstviewItems.Items.Add(listitem);
+                        }
+                      
+                    }
+                    else
+                    {
+                        Array branches = crt_group.Name.Split('.');
+                        this.opcBrowser.MoveTo(ref branches);
+                        this.opcBrowser.ShowLeafs(true);
+                        foreach (object turn in this.opcBrowser)
+                        {
+                            string name = turn.ToString();
+                            //crt_group.OPCItems.AddItem(name, this.itemHandleClient);
+
+                            ListViewItem listitem = new ListViewItem(new string[] {
+                            name,
+                            "",
+                            "",
+                            "",
+                            "",
+                            ""});
+                            this.lstviewItems.Items.Add(listitem);
+                        }
+
+                        this.opcBrowser.ShowLeafs(true);
+                        this.opcBrowser.MoveToRoot();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Error(ex);
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Connect("127.0.0.1", "KEPware.KEPServerEx.V4");
+          
+        }
+
+        internal class LogHelper
+        {
+            public static void Error(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        private void lstviewItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lbShow.Text = "";
+            if (lstviewItems.SelectedIndices!=null && lstviewItems.SelectedIndices.Count>0)
+            {
+                var x = lstviewItems.SelectedIndices;
+                foreach (var item in lstviewItems.Items[x[0]].SubItems)
+                {                  
+                    lbShow.Text += item.ToString() + "\r\n";
+                }
+               
+            } 
+        }
+    }
+}
